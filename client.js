5e75067f997ee978c34cc9bfc98c900f3f6db7a0
Jordan Newberry <jordan.newberry@gmail.com>
2020-11-09 21:19:23 -0600

    click logic is working. needs bells and whistles

--- client.js
+++ client.js
@@ -1,15 +1,16 @@
 console.log('Here are all the available people:', people);
 $(document).ready(readyNow);
 
+let personNumber = 0;
+
 function readyNow() {
     whoToClick();
-    let personNumber = randomNumber(0, people.length);
-    console.log(personNumber);
+    $('#pictureSpot').on('click', '.imgClick', handleClick) // click event handler
 
-    for (let i = 0; i < people.length; i++) {
+    for (let i = 0; i < people.length; i++) { // for-of loop creating dynamic divs with unique IDs
         let newDiv = `
         <div class="${i}">
-        <img src="https://github.com/${people[i].githubUsername}.png?size=250" alt="${people[i].name}">
+        <img class="imgClick" data-name="${people[i].githubUsername}" src="https://github.com/${people[i].githubUsername}.png?size=250" alt="${people[i].name}">
         </div>`;
         $('#pictureSpot').append(newDiv);
     };
@@ -20,45 +21,53 @@ function randomNumber(min, max) {
 }
 
 function handleClick() {
-    console.log('in handleClick');
+    let whatEva = $(this).data('name');
+    if (whatEva == people[personNumber].githubUsername) {
+        alert('You got it right! Go for another!');
+    } else {
+        alert('Keep on clicking!');
+    };
+    whoToClick();
+    
+
 }
 
 function whoToClick() {
-    let personNumber = randomNumber(0, people.length);
+    personNumber = randomNumber(0, people.length);
     console.log(personNumber);
     switch (personNumber) {
         case 0:
-            alert(`Click on: ${people[0].name}`);
+            alert(`Click on ${people[0].name}!`);
             break;
         case 1:
-            alert(`Click on: ${people[1].name}`);
+            alert(`Click on ${people[1].name}!`);
             break;
         case 2:
-            alert(`Click on: ${people[2].name}`);
+            alert(`Click on ${people[2].name}!`);
             break;
         case 3:
-            alert(`Click on: ${people[3].name}`);
+            alert(`Click on ${people[3].name}!`);
             break;
         case 4:
-            alert(`Click on: ${people[4].name}`);
+            alert(`Click on ${people[4].name}!`);
             break;
         case 5:
-            alert(`Click on: ${people[5].name}`);
+            alert(`Click on ${people[5].name}!`);
             break;
         case 6:
-            alert(`Click on: ${people[6].name}`);
+            alert(`Click on ${people[6].name}!`);
             break;
         case 7:
-            alert(`Click on: ${people[7].name}`);
+            alert(`Click on ${people[7].name}!`);
             break;
         case 8:
-            alert(`Click on: ${people[8].name}`);
+            alert(`Click on ${people[8].name}!`);
             break;
         case 9:
-            alert(`Click on: ${people[9].name}`);
+            alert(`Click on ${people[9].name}!`);
             break;
         case 10:
-            alert(`Click on: ${people[10].name}`);
+            alert(`Click on ${people[10].name}!`);
             break;
     }
 
